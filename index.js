25a542baa2eb0f20961ddc7941d99e89d8648384
huzarh <huzarh44@gmail.com>
2023-11-12 13:09:31 +0300

    build 3

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const { Server } = require("socket.io");
 
-const io = new Server(8000, {
-  cors: true,
-});
+const io = new Server(8000, { pingTimeout: 60000, cors: { origin: "*" } });
 
 const emailToSocketIdMap = new Map();
 const socketidToEmailMap = new Map();
